1801c84abf225111ca4fca6a399a5157d28bc9a2
Chris Stead <Chris.Stead@hunterindustries.com>
2017-03-23 11:52:07 -0700

    Initialized atom plugin

---
+++ spec/js-refactor-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import JsRefactorAtomView from '../lib/js-refactor-atom-view';
+
+describe('JsRefactorAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
